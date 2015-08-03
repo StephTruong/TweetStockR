a499ace2e0d184239bd940e48a21aab703d33abb
calhank <hank.m@berkeley.edu>
2015-08-02 21:53:01 -0400

    added default db name to getsqlite()

--- App/databases.py
+++ App/databases.py
@@ -2,13 +2,14 @@ import json
 import datetime
 import sqlite3 as sql
 import pymongo as pm
+from aws import get_s3_connection, write_string_to_key
 
-def getsqlite(name):
+def getsqlite(name='db/stocks.sqlite3'):
 	return sql.connect(name)
 
 def sqlite_setup(conn):
 	cursor = conn.cursor()
-	# cursor.execute("CREATE TABLE prices (id integer, name text, datetime timestamp, price real, cur_price real, fix_price real)")
+	cursor.execute("CREATE TABLE prices (id integer, name text, datetime timestamp, price real, cur_price real, fix_price real)")
 	cursor.execute("CREATE TABLE sentiment (id integer, score real, datetime timestamp, company text, retweet integer)")
 
 def insert_into_price_table(conn, ticker):
@@ -26,20 +27,22 @@ def insert_into_price_table(conn, ticker):
 
 def insert_into_sentiment_table(conn, cursor, tweet):
 	# dump sentiment to SQLite
-	cursor.execute("INSERT INTO sentiment VALUES (?,?,?,?)", data)
+	cursor.execute("INSERT INTO sentiment VALUES (?,?,?,?)", tweet)
 	conn.commit()
 
 def get_mongo_conn():
 	return pm.MongoClient()
 
-def dump_collection_to_s3(coll, bucket, keyname):
+def dump_collection_to_s3(coll, bucket, basename):
+	name = basename + "_" + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.json'
 	data = list(coll.find())
-	key = Key(bucket)
-	key.key = keyname
-	key.set_contents_from_string(json.dumps(data, ensure_ascii=False))
+	coll.drop()
+	write_string_to_key(bucket, name, json.dumps(data, ensure_ascii=False))
 
-def upsert_tweet(coll, tweet):
+def upsert_tweet(coll, tweet, bucket, basename):
 	status = coll.update( {'id': tweet['id']}, tweet, upsert=True )
+	if coll.count() > 1000:
+		dump_collection_to_s3(coll, bucket, 'tweetdump')
 	return status['updatedExisting']
 
 if __name__=="__main__":
